cdcab83ae39ad29dce9b400cebd471e85a02d1e4
mateogonzalezdw <mateogaelgonzalezbarila@gmail.com>
2025-05-26 15:18:02 -0300

    26/5.js

--- Mateo González/index.js
+++ Mateo González/index.js
@@ -1,5 +1,4 @@
-function solicitudAJAX() {
-    var url = "https://pokeapi.co/api/v2/pokemon/";
+var url = "https://pokeapi.co/api/v2/pokemon/";
     let tarjetas = document.querySelector("#nPokemon");
     var objXMLHttpRequest = new XMLHttpRequest();
   
@@ -88,3 +87,46 @@ function buscarPorNombre() {
     })
 }
 
+
+
+  function buscar() {
+    let tarjetas = document.querySelector("#ConteinerCard");
+    var data = document.querySelector("#nPokemon").data;
+    var busqueda = document.querySelector("#nPokemon").value - 1;
+    var url = data.results[busqueda].url;
+  
+    if (busqueda >= 0) {
+      var objXMLHttpRequest = new XMLHttpRequest();
+  
+      objXMLHttpRequest.onreadystatechange = function () {
+        if (objXMLHttpRequest.readyState === 4) {
+          if (objXMLHttpRequest.status === 200) {
+            let json = JSON.parse(objXMLHttpRequest.responseText);
+            let nombre = json.name;
+            let uriImg = json.sprites.other.home.front_default;
+            let html =
+              `<div class="card" style="width: 18rem;">
+    <img src="` +
+              uriImg +
+              `" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">` +
+              nombre +
+              `</h5>
+      <p class="card-text"></p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>`;
+            tarjetas.innerHTML = html;
+          } else {
+            alert("Error Code: " + objXMLHttpRequest.status);
+            alert("Error Message: " + objXMLHttpRequest.statusText);
+          }
+        }
+      };
+      objXMLHttpRequest.open("GET", url);
+      objXMLHttpRequest.send();
+    } else {
+      alert("Debe ingresar un numero de 1 a 20 para obtener un Pokemon valido");
+    }
+  }
